e84908baca236c7dde6e1027836e1bbfd44dc100
Abilashakuruva <kuruvaabilasha218@gmail.com>
2025-02-05 01:31:40 +0530

    adding useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import ListComponents from './container/components/ListComponents'
 import UserProfile from './container/components/UserProfile'
 
 const App = () => {
-  
-
-
+  const[valueA,setValueA]=useState(5);
+  const[valueB,setValueB]=useState(10);
   return (
+    <div>
+      <button onClick={()=>setValueA(valueA+5)}>Increment</button>
+      <button onClick={()=>setValueB(valueB+1)}>Decrement</button>
+      <ListComponents a={valueA} b={valueB}/>
     
-    <UserProfile/>
+    {/*<UserProfile/>*/}
+    </div>
 
 
   )
--- src/container/components/ListComponents.jsx
+++ src/container/components/ListComponents.jsx
@@ -7,8 +7,8 @@ const ListComponents = ({a,b}) => {
     
   return (
     <div>
-        <p>a:{a}</p>
-        <p>b:{b}</p>
+        <p>a: {a}</p>
+        <p>b: {b}</p>
         <p>Memoized Value:{memoizedValue}</p>
       
     </div>
